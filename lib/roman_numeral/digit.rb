d7a71f1849a3539302a397c9ee6fe8ffa8a98cda
Sam Livingston-Gray <geeksam@gmail.com>
2016-06-02 11:50:32 -0700

    Remove helper methods from public interface

--- lib/roman_numeral/digit.rb
+++ lib/roman_numeral/digit.rb
@@ -9,15 +9,19 @@ class RomanNumeral
   Digit = Struct.new(:representation, :base_10, :successor) do
     alias to_i base_10
 
-    def <(integer)
-      integer.div(base_10) > 0
-    end
-
     def subtractive(integer)
       head = subtractive_chunk(integer)
       tail = successor.subtractive(integer % base_10)
       head + tail
     end
+
+    protected
+
+    def <(integer)
+      integer.div(base_10) > 0
+    end
+
+    private
 
     def subtractive_chunk(integer)
       if self < integer
