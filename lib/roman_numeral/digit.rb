1bb4a237c1739f8a697c0a2f55c6270c7e0d8a50
Sam Livingston-Gray <geeksam@gmail.com>
2016-06-02 11:54:29 -0700

    Replace protected method with private one
    Because :< needs a 'self', it had to be protected instead of private.
    Changing the name for the concept allowed the helper to be private, and
    as a happy coincidence, it also communicates intent a bit more clearly.

--- lib/roman_numeral/digit.rb
+++ lib/roman_numeral/digit.rb
@@ -7,28 +7,24 @@ class RomanNumeral
   end
 
   Digit = Struct.new(:representation, :base_10, :successor) do
-    alias to_i base_10
-
     def subtractive(integer)
       head = subtractive_chunk(integer)
       tail = successor.subtractive(integer % base_10)
       head + tail
     end
 
-    protected
-
-    def <(integer)
-      integer.div(base_10) > 0
-    end
-
     private
 
     def subtractive_chunk(integer)
-      if self < integer
+      if needs_representation?( integer )
         representation * integer.div(base_10)
       else
         ""
       end
+    end
+
+    def needs_representation?(integer)
+      integer.div(base_10) > 0
     end
   end
 
